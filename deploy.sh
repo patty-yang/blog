3a6f691e7e781d691d09647e2165438565316c90
sunny-one <643017440@qq.com>
2024-01-09 13:06:28 +0800

    fix: 博客迁移

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+
+
+# 忽略错误
+set -e
+
+# 构建
+npm run build
+
+# 进入待发布的目录
+cd .vitepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果部署到 https://<USERNAME>.github.io
+git push -f git@github.com:xiaoYangHAHAHA/Sum-one.github.io.git master:gh-pages
+
+# 如果是部署到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
