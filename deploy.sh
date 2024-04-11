4451f42b937bbc25c317f4097f67eb1b5c29c923
basic <643017440@qq.com>
2024-04-11 13:53:20 +0800

    fix:

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-git push -f git@github.com:xiaoYangHAHAHA/Sum-one.github.io.git master:gh-pages
+git push -f git@github.com:SunnySeptemberBoy/SunnySeptemberBoy.github.io.git master:gh-pages
+# git push -f git@github.com:xiaoYangHAHAHA/Sum-one.github.io.git master:gh-pages
+
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
