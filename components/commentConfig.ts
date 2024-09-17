9b71e851bf990be7d2f654e584631c8d134fb7e9
basic <643017440@qq.com>
2024-09-17 09:19:28 +0800

    fix: 梳理下结构

---
+++ components/commentConfig.ts
@@ -0,0 +1,14 @@
+const clientId = 'Ov23li1WG4ZVErHQjhUW'
+const clientSecret = 'f450fd978317dcf928bd4fc8280315c440e167d8'
+const repo = 'blog'  // 存放评论的仓库名
+const owner = 'SunnySeptemberBoy' // github用户名
+const admin = 'SunnySeptemberBoy' // github管理者列表
+
+const commentConfig = {
+    clientID: clientId,
+    clientSecret: clientSecret,
+    repo: repo,
+    owner: owner,
+    admin: admin,
+}
+export default commentConfig
