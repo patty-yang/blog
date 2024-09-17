be8ce1b8edb3106d3105b8eb0c74d69f85c34804
basic <643017440@qq.com>
2024-09-17 09:19:28 +0800

    fix: 梳理下结构

--- components/Gitalk.vue
+++ components/Gitalk.vue
@@ -7,21 +7,19 @@
 import Gitalk from 'gitalk'
 import 'gitalk/dist/gitalk.css'
 
-import { onMounted } from 'vue'
+import {onMounted} from 'vue'
+
+import comment from "./commentConfig";
 
 onMounted(() => {
   initGitalk()
 })
 const initGitalk = () => {
   const commentConfig = {
-    clientID: 'Ov23li1WG4ZVErHQjhUW',
-    clientSecret: 'f450fd978317dcf928bd4fc8280315c440e167d8',
-    repo: 'blog', // 存放评论的仓库名
-    owner: 'SunnySeptemberBoy', // github用户名
-    admin: ['SunnySeptemberBoy'], // github管理者列表
+    ...comment,
     id: location.pathname,
     distractionFreeMode: false,
-    createIssueManually: true //如果当前页面没有相应的 isssue 且登录的用户属于 admin，则会自动创建 issue。如果设置为 true，则显示一个初始化页面，创建 issue 需要点击 init 按钮。
+    createIssueManually: true // 如果当前页面没有相应的 isssue 且登录的用户属于 admin，则会自动创建 issue。如果设置为 true，则显示一个初始化页面，创建 issue 需要点击 init 按钮。
   }
   const gitalk = new Gitalk(commentConfig)
   gitalk.render('gitalk-container')
