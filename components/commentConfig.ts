7f5d0fc48f4f3899a0d9f2ebaf74743c5c6eb50f
basic <643017440@qq.com>
2024-10-25 09:26:41 +0800

    fix: 修改 github 用户名

--- components/commentConfig.ts
+++ components/commentConfig.ts
@@ -1,14 +1,14 @@
-const clientId = 'Ov23li1WG4ZVErHQjhUW'
+const clientID = 'Ov23li1WG4ZVErHQjhUW'
 const clientSecret = 'f450fd978317dcf928bd4fc8280315c440e167d8'
 const repo = 'blog'  // 存放评论的仓库名
-const owner = 'SunnySeptemberBoy' // github用户名
-const admin = 'SunnySeptemberBoy' // github管理者列表
+const owner = 'patty-yang' // github用户名
+const admin = 'patty-yang' // github管理者列表
 
 const commentConfig = {
-    clientID: clientId,
-    clientSecret: clientSecret,
-    repo: repo,
-    owner: owner,
-    admin: admin,
+    clientID,
+    clientSecret,
+    repo,
+    owner,
+    admin,
 }
 export default commentConfig
