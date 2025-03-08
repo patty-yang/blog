ddd3e2b82c4a925485a2f90a794c4e5225c60fa3
patty-yang <643017440@qq.com>
2025-03-08 22:13:40 +0800

    refactor: 将css移入全局

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,13 @@
 import Theme from 'vitepress/theme'
 import Gitalk from '../../components/Gitalk.vue'
+
+import './common.css'
+
+import type {EnhanceAppContext} from 'vitepress'
+
 export default {
   ...Theme,
-  enhanceApp(ctx) {
+  enhanceApp(ctx: EnhanceAppContext) {
     Theme.enhanceApp(ctx)
     ctx.app.component('Gitalk', Gitalk)
     // app is an express instance
