0ab8b88d88c168110931f0dbe2c33ce4393d5b92
patty-yang <643017440@qq.com>
2025-03-09 01:35:36 +0800

    refactor: 添加 Gitalk 组件到主题布局中以增强文档评论功能

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,12 +1,18 @@
 import Theme from 'vitepress/theme'
+import { h } from 'vue'
 import Gitalk from '../../components/Gitalk.vue'
 
 import './common.css'
 
-import type {EnhanceAppContext} from 'vitepress'
+import type { EnhanceAppContext } from 'vitepress'
 
 export default {
   ...Theme,
+  Layout: () => {
+    return h(Theme.Layout, null, {
+      'doc-after': () => h(Gitalk)
+    })
+  },
   enhanceApp(ctx: EnhanceAppContext) {
     Theme.enhanceApp(ctx)
     ctx.app.component('Gitalk', Gitalk)
