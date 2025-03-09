4d152866271fe36df916c27a2e17af69eaef3629
patty-yang <643017440@qq.com>
2025-03-09 23:01:33 +0800

    refactor: replace Gitalk component with Giscus for enhanced commenting functionality

---
+++ components/Comment.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- params generate in https://giscus.app/zh-CN -->
+  <Giscus
+    v-if="showComment"
+    repo="patty-yang/blog"
+    repo-id="R_kgDOLCd40w"
+    category="Announcements"
+    category-id="DIC_kwDOLCd4084Cnupb"
+    mapping="pathname"
+    strict="1"
+    reactions-enabled="1"
+    emit-metadata="1"
+    input-position="bottom"
+    theme="preferred_color_scheme"
+    lang="zh-CN"
+    loading="lazy"
+    crossorigin="anonymous"
+  />
+</template>
+<script setup>
+import { nextTick, ref, watch } from 'vue'
+import { useRoute } from 'vitepress'
+import Giscus from '@giscus/vue'
+
+const route = useRoute()
+
+const showComment = ref(true)
+watch(
+  () => route.path,
+  (v) => {
+    showComment.value = false
+    nextTick(() => {
+      showComment.value = true
+    })
+  },
+  {
+    immediate: true
+  }
+)
+</script>
