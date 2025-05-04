1614934152dabac437ce78811569bd10e5c4689e
patty-yang <643017440@qq.com>
2025-05-04 09:19:51 +0800

    style: code format

--- demo/node/http/client.js
+++ demo/node/http/client.js
@@ -1,23 +1,22 @@
-const http = require("http");
+const http = require('http')
 
 const request = http.request(
-    'http://localhost:8080',
-    {
-      method: "GET"
-    },
-    resp => {
-      console.log("服务器响应的状态码", resp.statusCode);
-      console.log("服务器响应头", resp.headers);
-      let result = "";
-      resp.on("data", chunk => {
-        result += chunk.toString("utf-8");
-      });
+  'http://localhost:8080',
+  {
+    method: 'GET'
+  },
+  (resp) => {
+    console.log('服务器响应的状态码', resp.statusCode)
+    console.log('服务器响应头', resp.headers)
+    let result = ''
+    resp.on('data', (chunk) => {
+      result += chunk.toString('utf-8')
+    })
 
-      // resp.on("end", chunk => {
-        // console.log(JSON.parse(result));
-      // });
-    }
-);
+    // resp.on("end", chunk => {
+    // console.log(JSON.parse(result));
+    // });
+  }
+)
 
-request.end(); //表示消息体结束
-
+request.end() //表示消息体结束
